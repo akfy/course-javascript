31c2ad712f294890cc36342e914d3bb62ae8dfaa
Vladislav Turkevich <vladislav.turkevich@gmail.com>
2022-09-13 22:40:23 +0700

    dnd

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,44 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+let isDrag = false;
+document.addEventListener('mousedown', (e) => {
+  isDrag = true;
+});
 
-export function createDiv() {}
+document.addEventListener('mousemove', (e) => {
+  //console.log(e.target);
+  console.log(isDrag);
+  if (isDrag === true) {
+    e.target.style.left = e.pageX - e.target.offsetWidth / 2 + 'px';
+    e.target.style.top = e.pageY - e.target.offsetHeight / 2 + 'px';
+    console.log(e.pageX);
+    console.log(e.pageY);
+  }
+});
+
+document.addEventListener('mouseup', (e) => {
+  isDrag = false;
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+export function createDiv() {
+  const result = document.createElement('div');
+  result.classList.add('draggable-div');
+  result.style.height = getRandomInt(0, 500) + 'px';
+  result.style.width = getRandomInt(0, 500) + 'px';
+  result.style.top = getRandomInt(-500, 500) + 'px';
+  result.style.left = getRandomInt(-500, 500) + 'px';
+  const randomColor = getRandomInt(0, 16777216).toString(16);
+  result.style.backgroundColor = '#' + randomColor;
+  result.style.position = 'absolute';
+  return result;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
